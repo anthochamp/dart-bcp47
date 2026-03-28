b0af3d0f3497c49948f769e8149e3444f0ad30ad
Anthony Champagne <dev@anthonychampagne.fr>
2026-03-28 19:33:06 +0100

    fix markdownlint config

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -1,13 +1,19 @@
 module.exports = {
-	default: true,
+  config: {
+    default: true,
 
-	// MD013/line-length
-	// Line length : https://github.com/DavidAnson/markdownlint/blob/v0.35.0/doc/md013.md
-	MD013: false,
+    // MD013/line-length
+    // Line length : https://github.com/DavidAnson/markdownlint/blob/v0.35.0/doc/md013.md
+    MD013: false,
 
-	// MD024/no-duplicate-heading
-	// No duplicate headings : https://github.com/DavidAnson/markdownlint/blob/v0.38.0/doc/md024.md
-	MD024: {
-		siblings_only: true,
-	},
+    // MD024/no-duplicate-heading
+    // No duplicate headings : https://github.com/DavidAnson/markdownlint/blob/v0.38.0/doc/md024.md
+    MD024: {
+        siblings_only: true,
+    },
+  },
+
+  gitignore: true,
+
+  globs: ["**/*.{md,mdx}"],
 };
